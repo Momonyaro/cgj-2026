e29c716237c072deeebc3b47b8cf944ad66feaeb
Sebastian Alkstrand <Momonyaro@users.noreply.github.com>
2026-07-13 14:53:41 +0300

    added basic magician-on-a-stick

---
+++ src/shared/magician/magician.gd
@@ -0,0 +1,60 @@
+extends Node3D
+
+# Front and back relative to the camera
+@export var front_material: StandardMaterial3D
+@export var back_material: StandardMaterial3D
+@export var spin_duration: float = 0.2
+@export var default_texture: Texture2D
+@export_subgroup("Debug")
+@export var debug_enabled: bool = false
+@export var debug_textures: Array[Texture2D] = []
+
+@onready var face_root: Node3D = $ObjRoot/FaceRoot
+
+var texture: Texture2D: set = swap_texture
+
+var _tween: Tween
+var _spin_direction: int = 1 # -1 or 1
+var _current_index: int = 0
+
+# ---- Godot Events ----
+
+func _ready() -> void:
+	front_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, default_texture)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_pressed() and debug_enabled:
+		if event is InputEventKey:
+			if event.keycode == KEY_SPACE:
+				_current_index = (_current_index + 1) % debug_textures.size()
+				texture = debug_textures[_current_index]
+
+# ---- Public Functions ----
+
+func swap_texture(_texture: Texture2D) -> void:
+	# Set back buffer to current front before replacing it.
+	var current_front: Texture2D = front_material.get_texture(BaseMaterial3D.TEXTURE_ALBEDO)
+	back_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, current_front)
+	
+	# Set front to new texture and prepare for the flip anim
+	front_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, _texture)
+	_do_spin()
+
+# ---- Private Functions ---
+
+func _do_spin() -> void:
+	if _tween:
+		_tween.kill()
+		_tween = null
+	
+	_spin_direction = 1 if randf() >= 0.5 else -1
+	
+	face_root.rotation_degrees.y = 180 * _spin_direction
+	_tween = create_tween()
+	_tween.tween_method(_spin_tween, 0.0, 1.0, spin_duration)
+
+func _spin_tween(x: float) -> void:
+	if _spin_direction > 0:
+		face_root.rotation_degrees.y = 180.0 - (180.0 * x)
+	else:
+		face_root.rotation_degrees.y = -180.0 + (180.0 * x)
