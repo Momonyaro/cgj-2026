38cb3dac2e05c95140fe839f6346396286f8fc92
Benjamin <f.benjamin.larsson@gmail.com>
2026-07-15 21:52:54 +0300

    Spotlight score tickers

--- src/shared/magician/magician.gd
+++ src/shared/magician/magician.gd
@@ -1,3 +1,4 @@
+class_name Magician
 extends Node3D
 
 signal finished_spin
@@ -14,17 +15,23 @@ signal finished_spin
 
 @onready var face_root: Node3D = $BodyPivot/ObjRoot/FaceRoot
 
-var texture: Texture2D: set = swap_texture
+var texture: Texture2D:
+	set = swap_texture
 
 var _tween: Tween
 var _spin_direction: int = 1 # -1 or 1
 var _current_index: int = 0
 
+static var current_magician: Node3D
+
 # ---- Godot Events ----
+
 
 func _ready() -> void:
 	front_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, default_texture)
 	back_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, default_texture)
+	current_magician = self
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_pressed() and debug_enabled:
@@ -35,28 +42,31 @@ func _unhandled_input(event: InputEvent) -> void:
 
 # ---- Public Functions ----
 
+
 func swap_texture(_texture: Texture2D) -> void:
 	# Set back buffer to current front before replacing it.
 	var current_front: Texture2D = front_material.get_texture(BaseMaterial3D.TEXTURE_ALBEDO)
 	back_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, current_front)
-	
+
 	# Set front to new texture and prepare for the flip anim
 	front_material.set_texture(BaseMaterial3D.TEXTURE_ALBEDO, _texture)
 	_do_spin()
 
 # ---- Private Functions ---
 
+
 func _do_spin() -> void:
 	if _tween:
 		_tween.kill()
 		_tween = null
-	
+
 	_spin_direction = 1 if randf() >= 0.5 else -1
-	
+
 	face_root.rotation_degrees.y = 180 * _spin_direction
 	_tween = create_tween()
 	_tween.tween_method(_spin_tween, 0.0, 1.0, spin_duration)
 	_tween.finished.connect(finished_spin.emit)
+
 
 func _spin_tween(x: float) -> void:
 	if _spin_direction > 0:
---
+++ src/stage/lights/spot_light.gd
@@ -0,0 +1,26 @@
+extends WindableItem
+
+const PERFECT_ANGLE_THRESHOLD = cos(deg_to_rad(5))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	ScoreManager.set_ticker(name, magician_is_lit())
+
+
+func magician_is_lit() -> bool:
+	var light_direction := -global_basis.y
+	light_direction.z = 0
+	light_direction = light_direction.normalized()
+
+	var light_to_magician: Vector3
+	if Magician.current_magician:
+		light_to_magician = Magician.current_magician.global_position - global_position
+		light_to_magician.z = 0
+		light_to_magician = light_to_magician.normalized()
+
+		var dot := light_to_magician.dot(light_direction)
+		if dot >= PERFECT_ANGLE_THRESHOLD:
+			return true
+
+	return false
